027118e49abe24a54c78581330b8213dd423544b
Surya-0804 <surya.abothula@gmail.com>
2026-01-03 12:16:10 +0530

    feat:adding commit status

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import subprocess
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+@app.get("/status")
+def status():
+    """
+    this function return the last git commit hash """
+    try:
+        commit_id = (
+            subprocess.check_output(["git", "rev-parse", "HEAD"])
+            .decode("utf-8")
+            .strip()
+        )
+        return commit_id
+    except Exception:
+        return "unknown"
